e8f9d252f7523c38ad15548db5e6bb233df8d9f0
Akanksha Patel <p.akanksha94@gmail.com>
2020-03-30 02:29:04 -0400

    Changes in Wrapper.py

--- Wrapper.py
+++ Wrapper.py
@@ -24,7 +24,26 @@ def main():
 		# print("xxxxxxxxxxxxxxxxxxxx")
 		TwoFacesInVideo(cap)
 
-	if video == 
+	if video == 0 and method == 3:
+		parser = argparse.ArgumentParser(description='Texture Editing by PRN')
+
+		parser.add_argument('-i', '--image_path', default='TestImages/AFLW2000/image00081.jpg', type=str,
+		                    help='path to input image')
+		parser.add_argument('-r', '--ref_path', default='TestImages/trump.jpg', type=str, 
+		                    help='path to reference image(texture ref)')
+		parser.add_argument('-o', '--output_path', default='TestImages/output.jpg', type=str, 
+		                    help='path to save output')
+		parser.add_argument('--mode', default=1, type=int, 
+		                    help='ways to edit texture. 0 for modifying parts, 1 for changing whole')
+		parser.add_argument('--gpu', default='0', type=str, 
+                        help='set gpu id, -1 for CPU')
+
+	    # ---- init PRN
+	    os.environ['CUDA_VISIBLE_DEVICES'] = parser.parse_args().gpu # GPU number, -1 for CPU
+	    prn = PRN(is_dlib = True) 
+
+
+		texture_editing(prn, parser.parse_args())
 
 
 
